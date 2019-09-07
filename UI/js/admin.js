a5d1ee93ad491eb12acf02b2f0d641f1f84b06a8
UsanaseD <todiddy20@gmail.com>
2019-09-07 20:17:45 +0200

    feature(admin-page) that serves as admin dashboard in the system[start #168360137]

---
+++ UI/js/admin.js
@@ -0,0 +1,47 @@
+const defaultBtn = document.getElementById('defaultOpenbtn');
+const reportBtn = document.getElementById('reportedbtn');
+const ads = document.getElementById('ads');
+const report = document.getElementById('reported');
+const delete0btn = document.getElementById ('delete0');
+const delete1btn = document.getElementById ('delete1');
+const delete2btn = document.getElementById ('delete2');
+const delete3btn = document.getElementById ('delete3');
+
+
+
+window.addEventListener('load', (e) =>{
+  ads.style.display = 'block';
+  defaultBtn.style.background = 'blue';
+  reportBtn.style.background = '#32508e';
+  ads.style.background = '#f2f2f2';
+});
+
+reportBtn.addEventListener('click', (e) => {
+  ads.style.display = 'none';
+report.style.display = 'block';
+reportBtn.style.background = 'blue';
+defaultBtn.style.background = '#32508e';
+report.style.background= '#f2f2f2';
+});
+
+defaultBtn.addEventListener('click', (e) =>{
+  report.style.display = 'none';
+  ads.style.display = 'block';
+  reportBtn.style.background = '#32508e';
+  defaultBtn.style.background = 'blue';
+  ads.style.background= '#f2f2f2';
+});
+
+delete0btn.addEventListener('click', (e)=> {
+  alert("Comment Deleted succefully!");
+});
+
+delete1btn.addEventListener('click', (e)=> {
+  alert("Comment Deleted succefully!");
+});
+delete2btn.addEventListener('click', (e)=> {
+  alert("Post Deleted succefully!");
+});
+delete3btn.addEventListener('click',(e)=>{
+    alert("Post Deleted succefully!");
+});
