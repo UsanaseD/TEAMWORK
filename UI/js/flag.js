ec7f26a75c78ef7bb280ad171f57038a42324ef6
UsanaseD <todiddy20@gmail.com>
2019-09-07 21:11:37 +0200

    feature(flag-page) reporting page that allows users to flag sensitive info[start #168360257]

---
+++ UI/js/flag.js
@@ -0,0 +1,6 @@
+const updatebtn = document.getElementById('flag');
+
+updatebtn.addEventListener('click', (e)=> {
+    alert("Post Reported succefully!");
+    window.location.href= '../pages/home.html';
+});
