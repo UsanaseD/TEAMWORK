b9e7056d7a8ff1b99df43b7075e4d6bef4ec8477
UsanaseD <todiddy20@gmail.com>
2019-09-21 17:39:04 +0200

    adding a .ignore file

--- server/test/article_endpoint_testing.js
+++ server/test/article_endpoint_testing.js
@@ -27,6 +27,7 @@ describe('article endpoint testing', () => { // test case
       }).end((err, data) => {
         const { token } = data.body.data;
         global.myToken = token;
+        console.log(token)
         done();
       });
   });
