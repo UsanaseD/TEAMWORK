7ae9d2e5b4f9ceeb86cd5045b7f85cf1a3e508d9
UsanaseD <todiddy20@gmail.com>
2019-10-01 04:54:26 +0200

    bug(Ui-correction) to correct my UI layout[ends #168858820]

--- UI/js/home.js
+++ UI/js/home.js
@@ -23,21 +23,119 @@ const type = document.getElementById('ona');
 const close = document.getElementById('close');
 const less = document.getElementById('rctpst0');
 const read = document.getElementById('less0');
-const rdlss = document.getElementById('rd0');
+const rdlss = document.getElementById('rd');
 const read1 = document.getElementById('less1');
 const rdlss1 = document.getElementById('rd1');
 const less1 = document.getElementById('rctpst1');
-
+const title = document.getElementById('tit');
+const story = document.getElementById('stry');
+const head = document.getElementById('hd');
+const title1 = document.getElementById('tit1');
+const story1 = document.getElementById('stry1');
+const head1 = document.getElementById('hd1');
+const title2= document.getElementById('tit2');
+const story2 = document.getElementById('stry2');
+const head2 = document.getElementById('hd2');
+const rdlss2 = document.getElementById('rd2');
 
 window.addEventListener('load', (e) =>{
+type.style.display='none';
 container.style.display='block';
 container.style.width='100%';
 linkCarrer.style.display='none';
-flag.style.display ='none';
-flag1.style.display ='none';
-type.style.display ='none';
-less.rows = '10'
-less1.rows = '10'
+title.style.height='15%'
+head.style.height='100%'
+story.style.display='none'
+head.style.height='100%'
+title1.style.height='15%'
+head1.style.height='100%'
+story1.style.display='none'
+head1.style.height='100%'
+more.style.display='none'
+flag.style.display='none'
+more1.style.display='none'
+flag1.style.display='none'
+title2.style.height='15%'
+head2.style.height='100%'
+story2.style.display='none'
+more2.style.display='none'
+flag2.style.display='none'
+});
+postbtn.addEventListener('click',(e)=>{
+    alert("Post Updated succefully!");
+});
+delet0.addEventListener('click',(e)=>{
+    alert("Post Deleted succefully!");
+});
+delet1.addEventListener('click',(e)=>{
+    alert("Post Deleted succefully!");
+});
+delet2.addEventListener('click',(e)=>{
+    alert("Post Deleted succefully!");
+});
+rdlss2.addEventListener('mouseover',(e)=>{ 
+    title2.style.height='450px'
+    story2.style.display='block'
+    head2.style.height='30%'
+    rdlss2.textContent ='Read less....'
+    more2.style.display='block'
+});
+rdlss2.addEventListener('click',(e)=>{ 
+    title2.style.height='15%'
+    story2.style.display='none'
+    head2.style.height='100%'
+    rdlss2.textContent ='Read more....'
+    more2.style.display='none'
+    flag2.style.display='none'
+});
+more2.addEventListener('mouseover',(e)=>{
+    flag2.style.display='block'
+   });
+   more2.addEventListener('click',(e)=>{
+       flag2.style.display='none'
+      });
+more.addEventListener('mouseover',(e)=>{
+ flag.style.display='block'
+});
+more.addEventListener('click',(e)=>{
+    flag.style.display='none'
+   });
+   more1.addEventListener('mouseover',(e)=>{
+    flag1.style.display='block'
+   });
+   more1.addEventListener('click',(e)=>{
+       flag1.style.display='none'
+      });
+
+rdlss.addEventListener('mouseover',(e)=>{ 
+    title1.style.height='450px'
+    story1.style.display='block'
+    head1.style.height='30%'
+    rdlss.textContent ='Read less....'
+    more.style.display='block'
+});
+rdlss.addEventListener('click',(e)=>{ 
+    title1.style.height='15%'
+    story1.style.display='none'
+    head1.style.height='100%'
+    rdlss.textContent ='Read more....'
+    more.style.display='none'
+    flag.style.display='none'
+});
+rdlss1.addEventListener('mouseover',(e)=>{ 
+    title.style.height='450px'
+    story.style.display='block'
+    head.style.height='30%'
+    rdlss1.textContent ='Read less....'
+    more1.style.display='block'
+});
+rdlss1.addEventListener('click',(e)=>{ 
+    title.style.height='15%'
+    story.style.display='none'
+    head.style.height='100%'
+    rdlss1.textContent ='Read more....'
+    more1.style.display='none'
+    flag1.style.display='none'
 });
 
 close.addEventListener('click',(e)=>{
@@ -101,9 +199,7 @@ more1.addEventListener('click',(e)=>{
     flag1.style.display='none';
 });
 
-  delet0.addEventListener('click',(e)=>{
-    alert("Post Deleted succefully!");
-});
+
 
 delet1.addEventListener('click',(e)=>{
     alert("Post Deleted succefully!");
@@ -117,9 +213,7 @@ delet3.addEventListener('click',(e)=>{
     alert("Post Deleted succefully!");
 });
 
-postbtn.addEventListener('click',(e)=>{
-    alert("Post Updated succefully!");
-});
+
 
 read.addEventListener('mouseover',(e)=>{ 
     less.rows='24';
@@ -133,11 +227,7 @@ read.addEventListener('click',(e)=>{
    less.style.overflow='hidden'
 });
 
-read1.addEventListener('mouseover',(e)=>{ 
-    less1.rows='24';
-    rdlss1.textContent ='Read less....'
-    less1.style.overflow='scroll'
-});
+
 
 read1.addEventListener('click',(e)=>{ 
     less1.rows='7';
